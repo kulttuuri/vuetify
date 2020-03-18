243b3309653e5732c4fcb1144023a34e18f011a9
Aleksi Postari <aleksi@teachergaming.com>
2020-03-18 11:25:35 +0200

    Create fi.ts
    Added finnish translation.

---
+++ packages/vuetify/src/locale/fi.ts
@@ -0,0 +1,48 @@
+export default {
+  badge: 'Merkki',
+  close: 'Sulje',
+  dataIterator: {
+    noResultsText: 'Ei hakutuloksia',
+    loadingText: 'Ladataan...',
+  },
+  dataTable: {
+    itemsPerPageText: 'Rivejä sivulla:',
+    ariaLabel: {
+      sortDescending: ': Lajiteltu laskevaksi. Aktivoi poistaaksesi lajittelun.',
+      sortAscending: ': Lajiteltu nousevaksi. Aktivoi lajitellaksesi laskevaksi.',
+      sortNone: ': Ei lajiteltu. Aktivoi lajitellaksesi nousevaksi.',
+    },
+    sortBy: 'Lajittele',
+  },
+  dataFooter: {
+    itemsPerPageText: 'Rivejä sivulla:',
+    itemsPerPageAll: 'Kaikki',
+    nextPage: 'Seuraava sivu',
+    prevPage: 'Edellinen sivu',
+    firstPage: 'Ensimmäinen sivu',
+    lastPage: 'Viimeinen sivu',
+    pageText: '{0}-{1} / {2}',
+  },
+  datePicker: {
+    itemsSelected: '{0} valittu',
+  },
+  noDataText: 'Ei tietoa saatavilla',
+  carousel: {
+    prev: 'Edellinen kuva',
+    next: 'Seuraava kuva',
+    ariaLabel: {
+      delimiter: 'Karusellin dia {0} / {1}',
+    },
+  },
+  calendar: {
+    moreEvents: '{0} lisää',
+  },
+  fileInput: {
+    counter: '{0} tiedostoa',
+    counterSize: '{0} tiedostoa ({1} yhteensä)',
+  },
+  timePicker: {
+    am: 'AM',
+    pm: 'PM',
+  },
+}
